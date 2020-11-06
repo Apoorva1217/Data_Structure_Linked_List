3aedc58fd9453e10ac8e823f62f21839d9ce4d23
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-07 00:21:18 +0530

    Delete push.sh

--- DataStructureLinkedList/push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add Program.cs
-git commit -m "[Apoorva] Add . Create a simple Linked List of 56, 30 and 70"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
