cd07c7b100775719dd0e0359ea0dfe6f89099568
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-06 22:05:00 +0530

    [Apoorva] Add . Create a simple Linked List of 56, 30 and 70

---
+++ DataStructureLinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(56);
+            linkedList.AddFirst(30);
+            linkedList.AddFirst(70);
+        }
+    }
+}
